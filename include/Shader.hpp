fdfa3ca8ceaff68bc9f433a2bff5a41dda6f1bed
Sven Schneider <postwar@web.de>
2011-03-02 17:17:33 +0100

    included error messages in Shader class and added Qt shaders via preprocessor

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -1,7 +1,44 @@
 #ifndef SHADER_HPP_
 #define SHADER_HPP_
 
-/*
+#ifndef USE_QT_SHADERS
+
+#include <GL/glew.h>
+#include <string>
+
+class Shader {
+	public:
+		Shader();
+		virtual ~Shader();
+		/// @param fileVS File name of the vertex shader.
+		/// @param fileFS File name of the fragment shader.
+		void Create(const std::string &fileVS, const std::string &fileFS);
+		void Bind();
+		void UnBind();
+		std::string DebugInfo();
+	
+	private:
+		std::string ReadFile(const std::string &file) const;
+		std::string GetShaderInfoLog(GLuint obj) const;
+		std::string GetProgramInfoLog(GLuint obj) const;
+	
+	private:
+		GLuint m_VertexShader;
+		GLuint m_FragmentShader;
+		GLuint m_ShaderProgram;
+		std::string m_VertexShaderFile;
+		std::string m_FragmentShaderFile;
+		std::string m_VertexShaderInfoLog;
+		std::string m_FragmentShaderInfoLog;
+		std::string m_ProgramInfoLog;
+};
+
+
+
+#else
+
+
+
 #include <QGLShader>
 #include <QGLShaderProgram>
 #include <string>
@@ -27,35 +64,7 @@ class Shader {
 		std::string m_VertexShaderFile;
 		std::string m_FragmentShaderFile;
 };
-*/
 
-/*
-#include <GL/gl.h>
-#include <GL/glext.h>
-*/
-#include <GL/glew.h>
-#include <string>
-
-class Shader {
-	public:
-		Shader();
-		virtual ~Shader();
-		/// @param fileVS File name of the vertex shader.
-		/// @param fileFS File name of the fragment shader.
-		void Create(const std::string &fileVS, const std::string &fileFS);
-		void Bind();
-		void UnBind();
-		std::string DebugInfo();
-	
-	private:
-		std::string ReadFile(const std::string &file) const;
-	
-	private:
-		GLuint m_VertexShader;
-		GLuint m_FragmentShader;
-		GLuint m_ShaderProgram;
-		std::string m_VertexShaderFile;
-		std::string m_FragmentShaderFile;
-};
+#endif
 
 #endif
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -6,7 +6,187 @@
 #include <cmath>
 
 
-/*
+#ifndef USE_QT_SHADERS
+
+Shader::Shader() {
+}
+
+
+Shader::~Shader() {
+}
+
+
+void Shader::Create(const std::string &fileVS, const std::string &fileFS) {
+	m_VertexShaderFile = fileVS;
+	m_FragmentShaderFile = fileFS;
+
+	m_VertexShader = glCreateShader(GL_VERTEX_SHADER);
+	m_FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	std::string vs = ReadFile(fileVS);
+	std::string fs = ReadFile(fileFS);
+	
+	const char *pvs = vs.c_str();
+	const char *pfs = fs.c_str();
+
+	glShaderSource(m_VertexShader, 1, &pvs, NULL);
+	glShaderSource(m_FragmentShader, 1, &pfs, NULL);
+
+	glCompileShader(m_VertexShader);
+	glCompileShader(m_FragmentShader);
+	
+	m_VertexShaderInfoLog = GetShaderInfoLog(m_VertexShader);
+	m_FragmentShaderInfoLog = GetShaderInfoLog(m_FragmentShader);
+
+	m_ShaderProgram = glCreateProgram();
+
+	glAttachShader(m_ShaderProgram, m_VertexShader);
+	glAttachShader(m_ShaderProgram, m_FragmentShader);
+	
+	glDeleteShader(m_VertexShader);
+	glDeleteShader(m_FragmentShader);
+
+	glLinkProgram(m_ShaderProgram);
+
+	m_ProgramInfoLog = GetProgramInfoLog(m_ShaderProgram);	
+}
+
+
+void Shader::Bind() {
+	glUseProgram(m_ShaderProgram);
+	
+	GLint samplerLocation;
+	
+	samplerLocation = glGetUniformLocation(m_ShaderProgram, "Tex0");
+	if (samplerLocation != -1) {
+		glUniform1i(samplerLocation, 0);
+	} else {
+		// std::cerr << "Error: Find location 'Tex0'" << std::endl;
+	}
+	
+	samplerLocation = glGetUniformLocation(m_ShaderProgram, "Tex1");
+	if (samplerLocation != -1) {
+		glUniform1i(samplerLocation, 1);
+	} else {
+		// std::cerr << "Error: Find location 'Tex1'" << std::endl;
+	}
+	
+	samplerLocation = glGetUniformLocation(m_ShaderProgram, "Tex2");
+	if (samplerLocation != -1) {
+		glUniform1i(samplerLocation, 2);
+	} else {
+		// std::cerr << "Error: Find location 'Tex2'" << std::endl;
+	}
+}
+
+
+void Shader::UnBind() {
+	glUseProgram(0);
+}
+
+
+std::string Shader::DebugInfo() {
+	std::stringstream info;
+
+	info << "Vertex shader: " << m_VertexShaderFile << std::endl;
+	info << "Fragment shader: " << m_FragmentShaderFile << std::endl;
+	
+	if (!m_VertexShaderInfoLog.empty()) {
+		info << "Vertex shader info: ";
+		info << m_VertexShaderInfoLog << std::endl;
+	}
+	
+	if (!m_FragmentShaderInfoLog.empty()) {
+		info << "Fragment shader info: ";
+		info << m_FragmentShaderInfoLog << std::endl;
+	}
+	
+	if (!m_ProgramInfoLog.empty()) {
+		info << "Program info: ";
+		info << m_ProgramInfoLog << std::endl;
+	}
+	
+	// the shader program must be bound to query the information
+	glUseProgram(m_ShaderProgram);
+
+	GLint activeUniforms;
+	glGetProgramiv(m_ShaderProgram, GL_ACTIVE_UNIFORMS, &activeUniforms);
+	info << "Active uniforms: " << activeUniforms << std::endl;
+	
+	GLint activeUniformMaxLength;
+	glGetProgramiv(m_ShaderProgram, GL_ACTIVE_UNIFORM_MAX_LENGTH, &activeUniformMaxLength);
+	
+	for (int i = 0; i < activeUniforms; i++) {
+		GLsizei bufSize = activeUniformMaxLength;
+		GLsizei length;
+		GLuint index = i;
+		GLint size;
+		GLenum type;
+		char name[bufSize];
+		memset(name, bufSize, 0);
+	
+		glGetActiveUniform(m_ShaderProgram, index, bufSize, &length, &size, &type, name);
+		info << "Uniform " << i << ": " << name << std::endl;
+	}
+	
+	return info.str();
+}
+
+
+std::string Shader::ReadFile(const std::string &file) const {
+	std::ifstream fstr(file.c_str());
+	std::stringstream sstr;
+	std::string line;
+	
+	while (getline(fstr, line, '\n')) {
+		sstr << line << std::endl;
+	}
+
+	return sstr.str();
+}
+
+
+std::string Shader::GetShaderInfoLog(GLuint obj) const {
+	std::stringstream sstr;
+	int infologLength = 0;
+	int charsWritten = 0;
+
+	glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+
+	if (infologLength > 1) {
+		char infoLog[infologLength];
+		glGetShaderInfoLog(obj, infologLength, &charsWritten, infoLog);
+		
+		sstr << "Error: " << infoLog;
+	}
+	
+	return sstr.str();
+}
+
+
+std::string Shader::GetProgramInfoLog(GLuint obj) const {
+	std::stringstream sstr;
+	int infologLength = 0;
+	int charsWritten  = 0;
+
+	glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+
+	if (infologLength > 1) {
+		char infoLog[infologLength];
+		glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
+		
+		sstr << "Error: " << infoLog;
+	}
+	
+	return sstr.str();
+}
+
+
+
+#else
+
+
+
 Shader::Shader() : m_VertexShader(QGLShader::Vertex), m_FragmentShader(QGLShader::Fragment), m_ShaderProgram() {
 }
 
@@ -77,159 +257,6 @@ std::string Shader::DebugInfo() {
 	
 	return info.str();
 }
-*/
 
-
-
-void printShaderInfoLog(GLuint obj) {
-	int infologLength = 0;
-	int charsWritten = 0;
-
-	glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
-
-	if (infologLength > 1) {
-		char infoLog[infologLength];
-		glGetShaderInfoLog(obj, infologLength, &charsWritten, infoLog);
-		
-		std::cout << "Error: " << infoLog << std::endl;
-	}
-}
-
-
-void printProgramInfoLog(GLuint obj) {
-	int infologLength = 0;
-	int charsWritten  = 0;
-
-	glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
-
-	if (infologLength > 1) {
-		char infoLog[infologLength];
-		glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
-		
-		std::cout << "Error: " << infoLog << std::endl;
-	}
-}
-
-
-Shader::Shader() {
-}
-
-
-Shader::~Shader() {
-}
-
-
-void Shader::Create(const std::string &fileVS, const std::string &fileFS) {
-	m_VertexShaderFile = fileVS;
-	m_FragmentShaderFile = fileFS;
-
-	m_VertexShader = glCreateShader(GL_VERTEX_SHADER);
-	m_FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	std::string vs = ReadFile(fileVS);
-	std::string fs = ReadFile(fileFS);
-	
-	const char *pvs = vs.c_str();
-	const char *pfs = fs.c_str();
-
-	glShaderSource(m_VertexShader, 1, &pvs, NULL);
-	glShaderSource(m_FragmentShader, 1, &pfs, NULL);
-
-	glCompileShader(m_VertexShader);
-	glCompileShader(m_FragmentShader);
-	
-	printShaderInfoLog(m_VertexShader);
-	printShaderInfoLog(m_FragmentShader);
-
-	m_ShaderProgram = glCreateProgram();
-
-	glAttachShader(m_ShaderProgram, m_VertexShader);
-	glAttachShader(m_ShaderProgram, m_FragmentShader);
-	
-	glDeleteShader(m_VertexShader);
-	glDeleteShader(m_FragmentShader);
-
-	glLinkProgram(m_ShaderProgram);
-	
-	printProgramInfoLog(m_ShaderProgram);
-}
-
-
-void Shader::Bind() {
-	glUseProgram(m_ShaderProgram);
-	
-	GLint samplerLocation;
-	
-	samplerLocation = glGetUniformLocation(m_ShaderProgram, "Tex0");
-	if (samplerLocation != -1) {
-		glUniform1i(samplerLocation, 0);
-	} else {
-		// std::cerr << "Error: Find location 'Tex0'" << std::endl;
-	}
-	
-	samplerLocation = glGetUniformLocation(m_ShaderProgram, "Tex1");
-	if (samplerLocation != -1) {
-		glUniform1i(samplerLocation, 1);
-	} else {
-		// std::cerr << "Error: Find location 'Tex1'" << std::endl;
-	}
-	
-	samplerLocation = glGetUniformLocation(m_ShaderProgram, "Tex2");
-	if (samplerLocation != -1) {
-		glUniform1i(samplerLocation, 2);
-	} else {
-		// std::cerr << "Error: Find location 'Tex2'" << std::endl;
-	}
-}
-
-
-void Shader::UnBind() {
-	glUseProgram(0);
-}
-
-
-std::string Shader::DebugInfo() {
-	std::stringstream info;
-
-	info << "Vertex shader: " << m_VertexShaderFile << std::endl;
-	info << "Fragment shader: " << m_FragmentShaderFile << std::endl;
-	
-	// the shader program must be bound to query the information
-	glUseProgram(m_ShaderProgram);
-
-	GLint activeUniforms;
-	glGetProgramiv(m_ShaderProgram, GL_ACTIVE_UNIFORMS, &activeUniforms);
-	info << "Active uniforms: " << activeUniforms << std::endl;
-	
-	GLint activeUniformMaxLength;
-	glGetProgramiv(m_ShaderProgram, GL_ACTIVE_UNIFORM_MAX_LENGTH, &activeUniformMaxLength);
-	
-	for (int i = 0; i < activeUniforms; i++) {
-		GLsizei bufSize = activeUniformMaxLength;
-		GLsizei length;
-		GLuint index = i;
-		GLint size;
-		GLenum type;
-		char name[bufSize];
-		memset(name, bufSize, 0);
-	
-		glGetActiveUniform(m_ShaderProgram, index, bufSize, &length, &size, &type, name);
-		info << "Uniform " << i << ": " << name << std::endl;
-	}
-	
-	return info.str();
-}
-
-
-std::string Shader::ReadFile(const std::string &file) const {
-	std::ifstream fstr(file.c_str());
-	std::stringstream sstr;
-	std::string line;
-	
-	while (getline(fstr, line, '\n')) {
-		sstr << line << std::endl;
-	}
-
-	return sstr.str();
-}
+#endif
 
