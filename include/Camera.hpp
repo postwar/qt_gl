3822af8928c3f61602d61404fa4be2f340ea9f1d
Sven Schneider <postwar@web.de>
2011-03-09 12:16:20 +0100

    move gl related functions in own member function

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,6 +1,8 @@
 #ifndef CAMERA_HPP_
 #define CAMERA_HPP_
 
+#include "Vector.hpp"
+
 class Camera {
 	public:
 		Camera();
@@ -8,18 +10,19 @@ class Camera {
 		void Resize(int width, int height);
 		void Move(float x, float y, float z);
 		void Rotate(float x, float y, float z);
+		const Vector3 &GetPosition() const;
+		const Vector3 &GetRotation() const;
+		void Apply() const;
 	
 	private:
 		Camera(const Camera &camera);
 		Camera &operator =(const Camera &camera);
 	
 	private:
-		float m_PositionX;
-		float m_PositionY;
-		float m_PositionZ;
-		float m_RotationX;
-		float m_RotationY;
-		float m_RotationZ;
+		Vector3 m_Position;
+		Vector3 m_Rotation;
+		int m_ViewportWidth;
+		int m_ViewportHeight;
 };
 
 #endif
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -3,6 +3,12 @@
 
 
 Camera::Camera() {
+	m_Position.X = 0.0;
+	m_Position.Y = 0.0;
+	m_Position.Z = 0.0;
+	m_Rotation.X = 0.0;
+	m_Rotation.Y = 0.0;
+	m_Rotation.Z = 0.0;
 }
 
 
@@ -11,26 +17,51 @@ Camera::~Camera() {
 
 
 void Camera::Resize(int width, int height) {
-	glMatrixMode(GL_PROJECTION);
-	
-	glLoadIdentity();
-	glViewport(0, 0, width, height);
-	glOrtho(-width / 2.0, width / 2.0, -height / 2.0, height / 2.0, 0, 512);
-	// glFrustum(-width / 2.0, width / 2.0, -height / 2.0, height / 2.0, 0, 128);
-	// glFrustum(-1.0, 1.0, -1.0, 1.0, 0, 128);
-	
-	glMatrixMode(GL_MODELVIEW);
+	m_ViewportWidth = width;
+	m_ViewportHeight = height;
 }
 
 
 void Camera::Move(float x, float y, float z) {
-	glTranslatef(x, y, z);
+	m_Position.X += x;
+	m_Position.Y += y;
+	m_Position.Z += z;
 }
 
 
 void Camera::Rotate(float x, float y, float z) {
-	glRotatef(x, 1.0, 0.0, 0.0);
-	glRotatef(y, 0.0, 1.0, 0.0);
-	glRotatef(z, 0.0, 0.0, 1.0);
+	m_Rotation.X += x;
+	m_Rotation.Y += y;
+	m_Rotation.Z += z;
+}
+
+
+const Vector3 &Camera::GetPosition() const {
+	return m_Position;
+}
+
+
+const Vector3 &Camera::GetRotation() const {
+	return m_Rotation;
+}
+
+
+void Camera::Apply() const {
+	glMatrixMode(GL_PROJECTION);
+	
+	glLoadIdentity();
+	glViewport(0, 0, m_ViewportWidth, m_ViewportHeight);
+	glOrtho(-m_ViewportWidth / 2.0, m_ViewportWidth / 2.0, -m_ViewportHeight / 2.0, m_ViewportHeight / 2.0, 0.0, 512.0);
+	// glFrustum(-m_ViewportWidth / 2.0, m_ViewportWidth / 2.0, -m_ViewportHeight / 2.0, m_ViewportHeight / 2.0, 0, 128);
+	// glFrustum(-1.0, 1.0, -1.0, 1.0, 0, 128);
+
+	
+	glMatrixMode(GL_MODELVIEW);
+
+	glTranslatef(m_Position.X, m_Position.Y, m_Position.Z);
+	
+	glRotatef(m_Rotation.X, 1.0, 0.0, 0.0);
+	glRotatef(m_Rotation.Y, 0.0, 1.0, 0.0);
+	glRotatef(m_Rotation.Z, 0.0, 0.0, 1.0);
 }
 
